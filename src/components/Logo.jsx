9e1ee7b306da324eca8e5f51b1df5dfa2f8af1ba
Utku Kaynar <utku@buck.ai>
2023-04-05 22:09:06 +0300

    Add mobile logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,5 +1,9 @@
 import Image from 'next/image'
 
+export function Logomark(props) {
+  return <Image width="40" height="40" src="/logo.png" />
+}
+
 export function Logo(props) {
   return <Image width="151" height="40" src="/logo.png" />
 }
