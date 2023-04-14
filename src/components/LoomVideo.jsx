e5af3543255352f6fca443d3c97e33f67f4259a3
Utku Kaynar <utku@buck.ai>
2023-04-14 23:26:03 +0300

    Update LoomVideo component

--- src/components/LoomVideo.jsx
+++ src/components/LoomVideo.jsx
@@ -1,7 +1,12 @@
 export function LoomVideo({ source }) {
   return (
     <div className="relative aspect-video py-4">
-      <iframe src={source} frameborder="0" allowFullScreen className="absolute inset-0 w-full h-full"></iframe>
+      <iframe
+        src={source}
+        frameBorder="0"
+        allowFullScreen
+        className="absolute inset-0 h-full w-full"
+      ></iframe>
     </div>
   )
 }
